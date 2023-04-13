9239b636d2b7a2df2fa02e00cbcfcbb121fbd0e5
LideLiang <125731291+hourrfpeter@users.noreply.github.com>
2023-04-13 16:35:47 +0800

    Add files via upload

---
+++ day0414/src/work1.java
@@ -0,0 +1,9 @@
+public class work1 {
+    public static void main(String[] args) {
+        int b=1;
+        for (int a=1;a<=5;a++){
+            b=b*a;
+        }
+        System.out.println(b);
+    }
+}
